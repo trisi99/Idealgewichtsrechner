b870ed0dec3730f25eee31016b8c69b85a7d2302
trisi99 <trisi.buschi@gmail.com>
2023-10-30 14:28:05 +0100

    Projekt fertiggestellt

---
+++ README.md
@@ -0,0 +1,26 @@
+# Projekt Idealgewichtsrechner
+
+### Projektbeschreibung
+
+- Ausrechnen des Idealgewichts für einen breiten Körperbau
+- Ausrechnen des Idealgewichts für einen schmalen Körperbau
+- Rechnet das Idealgewicht anhand der Körpergröße und des Alters aus
+- optimiert für 1440x900px
+
+Hier geht's zur Live-Demo:
+
+https://trisi99.github.io/Idealgewichtsrechner/
+
+### Tools/Technologien
+
+1. HTML
+2. CSS
+3. JavaScript
+
+### Projekt-Screenshot
+
+![screenshot_preview](./assets/img/Bildschirmfoto%202023-10-30%20um%2014.19.49.png)
+
+### Screenshot_PAP
+
+![screenshot](./assets/img/PAP.png)
---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+const size = document.getElementById('size')
+const age = document.getElementById('age')
+const big = document.getElementById('big')
+const thin = document.getElementById('thin')
+const p =document.getElementById('p')
+
+function funcOne() {
+    
+    const formThin = (Number(size.value)- 100) + (Number(age.value)/10)*0.9*0.9
+    const formBig = (Number(size.value)- 100) + (Number(age.value)/10)*0.9*1.1
+    if(thin.checked){
+        p.innerHTML = "Dein Idealgewicht ist " + formThin.toFixed(2) + " kg"
+    } else if (big.checked){
+        p.innerHTML = "Dein Idealgewicht ist " + formBig.toFixed(2) + " kg"
+    } else {
+        p.style.color = "red"
+        p.innerHTML = "ERROR"
+    }
+
+    
+}
