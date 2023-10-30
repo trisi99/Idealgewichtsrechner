45029c01b64f02159d2bc63d9823926ae56316c2
trisi99 <trisi.buschi@gmail.com>
2023-10-30 14:44:11 +0100

    formel angepasst

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,8 @@ const p =document.getElementById('p')
 
 function funcOne() {
     
-    const formThin = (Number(size.value)- 100) + (Number(age.value)/10)*0.9*0.9
-    const formBig = (Number(size.value)- 100) + (Number(age.value)/10)*0.9*1.1
+    const formThin = [(Number(size.value)- 100) + (Number(age.value)/10)]*0.9*0.9
+    const formBig = [(Number(size.value)- 100) + (Number(age.value)/10)]*0.9*1.1
     if(thin.checked){
         p.innerHTML = "Dein Idealgewicht ist " + formThin.toFixed(2) + " kg"
     } else if (big.checked){
